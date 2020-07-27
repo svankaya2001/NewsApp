b66a024935f7a05c11caf201cfb93fd3d4c5a919
Shreyas <svankayalapati@ucdavis.edu>
2020-07-27 01:30:49 -0700

    Added Procfile and requirements.txt

--- newsweb/urls.py
+++ newsweb/urls.py
@@ -4,6 +4,6 @@ from . import views
 from django.views.generic.base import TemplateView
 
 urlpatterns = [
-    path('', TemplateView.as_view(template_name='news.html'), name='news'),
+
     path('', views.news)
 ]
